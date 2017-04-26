eb02d4c1877859ba7d9791cb882a907e209628e0
雪の下千尋 <widowmaker@zWidowmaker.local>
2017-04-26 12:49:11 +0800

    Initial commit

---
+++ src/main/java/com/widowmaker/www/GirldemoApplication.java
@@ -0,0 +1,12 @@
+package com.widowmaker.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirldemoApplication.class, args);
+	}
+}
